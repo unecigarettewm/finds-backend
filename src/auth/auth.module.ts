ea89582466b1e30c3099e45c6baf0596640a5122
jacobbinnie <4302819+unecigarettewm@users.noreply.github.com>
2024-01-21 17:48:22 -0500

    feat: add JWT Guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,17 @@ import { UsersService } from 'src/public/users/users.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
 import { LocalStrategy } from './strategies/local-strategy';
+import { JwtStrategy } from './strategies/jwt-strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UsersService, PrismaService, LocalStrategy],
+  providers: [
+    AuthService,
+    UsersService,
+    PrismaService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
   imports: [
     JwtModule.register({
       secret: `${process.env.JWT_SECRET}`,
--- src/public/finds/finds.controller.ts
+++ src/public/finds/finds.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { FindsService } from './finds.service';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @ApiTags('finds')
 @Controller('finds')
 export class FindsController {
   constructor(private readonly findsService: FindsService) {}
 
+  @UseGuards(JwtGuard)
   @Get('all')
   async allFinds() {
     return this.findsService.getAllFinds();
