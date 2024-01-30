8d513241f33ca11fdb1f9541897ee3d522dc5260
jacobbinnie <4302819+unecigarettewm@users.noreply.github.com>
2024-01-29 21:21:28 -0500

    feat: ensure profiles returned in search have username and are not deleted

--- src/public/search/search.service.ts
+++ src/public/search/search.service.ts
@@ -20,6 +20,9 @@ export class SearchService {
             username: {
               not: null,
             },
+            deleted_at: {
+              equals: null,
+            },
           },
         ],
       },
