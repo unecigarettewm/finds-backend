cf3d15c309b0df3d78a3aa0e14dacef50a800f27
jacobbinnie <4302819+unecigarettewm@users.noreply.github.com>
2024-02-01 13:01:30 -0500

    feat: add upload endpoints with support for heic images

--- src/public/upload/upload.controller.ts
+++ src/public/upload/upload.controller.ts
@@ -1,32 +1,33 @@
 import {
   Controller,
-  FileTypeValidator,
   MaxFileSizeValidator,
   ParseFilePipe,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ReqUser, ReqUserType } from 'src/auth/util/user.decorator';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
+  @UseGuards(JwtGuard)
   @Post()
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(
+    @ReqUser() user: ReqUserType,
     @UploadedFile(
       new ParseFilePipe({
-        validators: [
-          // new MaxFileSizeValidator({ maxSize: 1000 }),
-          // new FileTypeValidator({ fileType: 'image/jpeg' }),
-        ],
+        validators: [new MaxFileSizeValidator({ maxSize: 10000000 })],
       }),
     )
     file: Express.Multer.File,
   ) {
-    await this.uploadService.upload(file.originalname, file.buffer);
+    await this.uploadService.upload(file, user.userId.id);
   }
 }
--- src/public/upload/upload.service.ts
+++ src/public/upload/upload.service.ts
@@ -1,6 +1,7 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class UploadService {
@@ -10,12 +11,43 @@ export class UploadService {
 
   constructor(private readonly configService: ConfigService) {}
 
-  async upload(fileName: string, file: Buffer) {
+  convert = require('heic-convert');
+  fs = require('fs');
+
+  async heicToJpeg(file: Buffer) {
+    const outputBuffer = await this.convert({
+      buffer: file, // the HEIC file buffer
+      format: 'JPEG', // output format
+      quality: 1, // the jpeg compression quality, between 0 and 1
+    });
+
+    return outputBuffer as Buffer;
+  }
+
+  async upload(file: Express.Multer.File, userId: number) {
+    if (!file.mimetype.startsWith('image')) {
+      throw new Error('File is not an image');
+    }
+
+    let readyImage = file.buffer;
+
+    const isHeic = file.mimetype === 'image/heic';
+
+    if (isHeic) {
+      const convertedImage = await this.heicToJpeg(file.buffer);
+      readyImage = convertedImage; // Use the converted image buffer
+    }
+
+    const processedImage = await sharp(readyImage)
+      .resize(1000)
+      .toFormat('jpeg')
+      .toBuffer();
+
     await this.s3Client.send(
       new PutObjectCommand({
         Bucket: 'find-image-uploads',
-        Key: fileName,
-        Body: file,
+        Key: userId + Date.now() + Math.floor(Math.random() * 6) + '.jpeg',
+        Body: processedImage,
       }),
     );
   }
