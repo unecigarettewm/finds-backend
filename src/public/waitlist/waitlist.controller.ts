3126b1ef880a5d747c39a44f67bc7a50aa15ae0a
jacobbinnie <4302819+unecigarettewm@users.noreply.github.com>
2024-02-16 14:51:53 -0500

    feat: add waitlist api tag

--- src/public/waitlist/waitlist.controller.ts
+++ src/public/waitlist/waitlist.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { WaitlistService } from './waitlist.service';
 import { WaitlistSignupDto } from './dto/waitlistSignup.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('waitlist')
 @Controller('waitlist')
 export class WaitlistController {
   constructor(private readonly waitlistService: WaitlistService) {}
