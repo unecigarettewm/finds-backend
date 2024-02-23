20aa515be0e33fdf7e0960c6f4e6d56f17f2caa9
jacobbinnie <4302819+unecigarettewm@users.noreply.github.com>
2024-02-23 09:41:09 -0500

    feat: add controller for likes

--- src/public/likes/likes.controller.ts
+++ src/public/likes/likes.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
 import { LikesService } from './likes.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ReqUser, ReqUserType } from 'src/auth/util/user.decorator';
+import { UserLikeDto } from './dto/userLike.dto';
+import { SkipThrottle } from '@nestjs/throttler';
+import { ActiveLikeDto } from './dto/activeLike.dto';
 
+@ApiTags('likes')
 @Controller('likes')
 export class LikesController {
-  constructor(private readonly likesService: LikesService) {}
+  logger: Logger;
+
+  constructor(private readonly likesService: LikesService) {
+    this.logger = new Logger();
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtGuard)
+  @Post('user-likes')
+  async getUserLikes(@ReqUser() user: ReqUserType): Promise<UserLikeDto[]> {
+    return this.likesService.getUserLikes(user.userId.id);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtGuard)
+  @SkipThrottle()
+  @Post('find-user-like')
+  async getFindUserLike(
+    @ReqUser() user: ReqUserType,
+    @Body() { id }: { id: number },
+  ): Promise<ActiveLikeDto> {
+    return this.likesService.getFindUserLike(id, user.userId.id);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtGuard)
+  @Post('update-like')
+  async updateLike(
+    @ReqUser() user: ReqUserType,
+    @Body() { id }: { id: number },
+  ): Promise<ActiveLikeDto> {
+    return this.likesService.updateLike(id, user.userId.id);
+  }
 }
